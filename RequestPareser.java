45a954b9e6c9743ec929fddf218adb973f6f1ced
Samuel <jawahar.samuel@gmail.com>
2018-09-19 16:12:56 +0530

    It is parsing the url

---
+++ RequestPareser.java
@@ -0,0 +1,13 @@
+package arK.server;
+
+public class RequestPareser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void parseRequest() {
+
+	}
+}
